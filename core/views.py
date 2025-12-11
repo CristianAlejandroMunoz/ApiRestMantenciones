38bea78d79d44613756d4b6c3186bf1dff6fa928
CristianMuñoz <cristian.munoz236@inacapmail.cl>
2025-12-11 01:47:00 -0300

    feat: autenticación JWT y reglas de acceso mediante permisos DRF

--- core/views.py
+++ core/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -16,38 +13,37 @@ from .serializers import (
 )
 
 
-# ----------------------------
-#  Endpoint de prueba (Pauta)
-# ----------------------------
+# Endpoint de estado obligatorio por la pauta
 @api_view(["GET"])
 def status(request):
     return Response({"status": "API funcionando correctamente"})
 
 
-# ----------------------------
-#       VIEWSETS CRUD
-# ----------------------------
-
 class EmpresaViewSet(viewsets.ModelViewSet):
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class EquipoViewSet(viewsets.ModelViewSet):
     queryset = Equipo.objects.all()
     serializer_class = EquipoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class TecnicoViewSet(viewsets.ModelViewSet):
     queryset = Tecnico.objects.all()
     serializer_class = TecnicoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class PlanMantencionViewSet(viewsets.ModelViewSet):
     queryset = PlanMantencion.objects.all()
     serializer_class = PlanMantencionSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class OrdenTrabajoViewSet(viewsets.ModelViewSet):
     queryset = OrdenTrabajo.objects.all()
     serializer_class = OrdenTrabajoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
